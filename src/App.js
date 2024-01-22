6b9e7297bca90d2985bf77f567a550b6db5c0bcb
Diego Ferraz <diego_ferraz@mckinsey.com>
2024-01-22 12:27:17 -0300

    feat: adds reservation funcionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 
 import Home from "./pages/Home";
+import Bookings from "./pages/Bookings";
+import BookingConfirmation from "./pages/BookingConfirmation";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <Layout>
       <Routes>
-        <Route path={"/"} element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/bookings" element={<Bookings />} />
+        <Route path="/confirmed-booking" element={<BookingConfirmation />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -3,7 +3,7 @@ import "./not-found.css";
 const NotFound = () => {
   return (
     <div className="container notFound">
-      <h2>Page not found</h2>
+      <h2>Page not found, or it is under construction.</h2>
     </div>
   );
 };
